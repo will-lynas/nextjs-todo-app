a67f1237e9c3a5dc3522acc83e77b61272475af8
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-03-24 22:27:25 +0000

    Fix broken opacity
    Tailwind 2 syntax instead of 3

--- app/components/ConfirmModal.tsx
+++ app/components/ConfirmModal.tsx
@@ -12,7 +12,7 @@ export default function ConfirmModal({
   todoTitle,
 }: ConfirmModalProps) {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black/50  flex items-center justify-center z-50">
       <div className="bg-neutral-900 p-6 rounded-lg max-w-md w-full mx-4">
         <h2 className="text-xl font-semibold mb-4 text-white">Delete Todo</h2>
         <p className="text-white mb-6">
