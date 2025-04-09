569dd9e3a142e4fd4cb6acaf0d21197af51cc545
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-04-09 21:40:08 +0100

    Add a migration

---
+++ migrations/1744230401026_make-todos.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("todos", {
+    id: "id",
+    title: { type: "varchar(1000)", notNull: true },
+    completed: { type: "boolean", notNull: true, default: false },
+    createdAt: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    userId: { type: "varchar(255)", notNull: true },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
