dc05f8b25d64bdf71e88c653e9dbb53f343eba19
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-04-09 22:12:19 +0100

    Migration for prisma data

---
+++ migrations/1744232646594_transfer-prisma-data.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`
+        INSERT INTO todos (user_id, title, completed, created_at)
+        SELECT "userId", "title", "completed", "createdAt"
+        FROM "Todo"
+    `);
+  pgm.dropTable("Todo");
+  pgm.dropTable("_prisma_migrations");
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
